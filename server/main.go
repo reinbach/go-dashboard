49a85124d2a63cbd7178b82e69642c38c68120f3
Greg Reinbach <greg@reinbach.com>
2015-03-18 20:03:19 -0400

    added rough code to send data from server for line chart

--- server/main.go
+++ server/main.go
@@ -1,19 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
 
 	"github.com/googollee/go-socket.io"
 )
 
+type LineChart struct {
+	Number int
+	Amount float64
+	Length int
+	Max    float64
+	Min    float64
+}
+
+func ChatHandler(so socketio.Socket, msg string) {
+	so.BroadcastTo("chat", "chat message", msg)
+}
+
+func StreamData(so socketio.Socket, l LineChart) {
+	r := rand.New(rand.NewSource(99))
+	var end int
+	end = l.Number + l.Length
+	for i := l.Number; i < end; i++ {
+		n := r.Float64() * l.Max
+		d := LineChart{
+			Number: i,
+			Amount: n,
+		}
+		j, err := json.Marshal(d)
+		if err != nil {
+			log.Println("Failed to Marshal message: ", err)
+		}
+		so.Emit("chart stream", string(j))
+	}
+}
+
+func ChartHandler(so socketio.Socket, msg string) {
+	d := LineChart{}
+	err := json.Unmarshal([]byte(msg), &d)
+	if err != nil {
+		log.Println("Unknown message: ", err)
+	}
+	StreamData(so, d)
+}
+
 func SocketHandler(so socketio.Socket) {
-	log.Println("connectioned")
+	log.Println("connected")
+
+	// chat
 	so.Join("chat")
-	so.On("chat message", func(msg string) {
-		log.Println("emit: ", so.Emit("chat message", msg))
-		so.BroadcastTo("chat", "chat message", msg)
-	})
+	so.On("chat message", ChatHandler)
+
+	// line chart simple
+	so.Join("chart")
+	so.On("chart stream", ChartHandler)
+
 	so.On("disconnection", func() {
 		log.Println("disconnected")
 	})
